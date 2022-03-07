08ed278bf0412b11ceb18a6d5093b25da5debde5
Sashank Silwal <sashanksilwal@hotmail.com>
2022-03-07 14:43:41 +0400

    added 3rd image

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const cookieSession = require('cookie-session');
 
 const app = express();
+
+app.set('trust proxy', 1);
+
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['fsafasdfdsafdsfsfsad', 'asdfasdfasgafdgrt423r'],
+  })
+);
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
